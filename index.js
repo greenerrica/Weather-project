5dbd05024f2ecf2f9d1a4ae5bde2ea92f356040c
greenerrica <greenerrica@gmail.com>
2023-03-10 09:01:26 -0600

    Update index.js
    added semi colon to the end

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function getforcast(coordinates) {
 console.log(coordinates);
 let apiKey = "3f6be1c407b0d9d1933561808db358ba";
 let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-console.log(apiURl)
+console.log(apiURl);
 }
 
 function showTemp(response) {
