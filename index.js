c54ec50a961b379472e9a6540c66b7c61bc06cf0
greenerrica <greenerrica@gmail.com>
2023-03-11 07:51:19 -0600

    Update index.js
    added a new function and new api

--- index.js
+++ index.js
@@ -28,11 +28,15 @@ function searchCity(city) {
   axios.get(apiURL).then(showTemp);
 }
 
+function displayForecast(response){
+  console.log(response.data)
+}
+
 function getforcast(coordinates) {
 console.log(coordinates);
 let apiKey = "3f6be1c407b0d9d1933561808db358ba";
-let apiURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
-console.log(apiURL);
+let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+axios.get(apiURL).then(displayForecast);
 }
 
 function showTemp(response) {
